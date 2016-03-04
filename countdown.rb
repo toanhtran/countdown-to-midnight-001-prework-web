28b4a2bf8ae67daaeb1f036c7495392b9fa46683
ToAnh Tran <toanh.t.tran@gmail.com>
2016-03-04 14:00:55 +0900

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(num_sec)
+  while num_sec > 0
+    puts "#{num_sec} SECOND(S)!"
+    num_sec -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_sec)
+  sleep num_sec
 end
